6d7154bb6b4277810926914805fce3ab567d63ff
Alhakeem <alhakeem.prof@gmail.com>
2018-11-03 23:10:07 +0300

    Add Vuetifyjs

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
+import '@/plugins/vuetify'
 import App from '@/App.vue'
-
 import router from '@/router'
 import $backend from '@/backend'
 Vue.prototype.$backend = $backend
 Vue.config.productionTip = false
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' 
+Vue.use(Vuetify)
 // Vue.use(VueRouter)
 
 const vue = new Vue({
