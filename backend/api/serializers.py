4a0e633b66d6f3dfe466abade9a6e0b72c8431ea
YaserAlnajjar <kasseryasser@windowslive.com>
2019-08-07 23:28:24 +0300

    Add debtor by admin

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,9 +8,17 @@ class DebtorSerializer(serializers.ModelSerializer):
     overdue_invoices_count = serializers.ReadOnlyField()
     paid_invoices_count = serializers.ReadOnlyField()
 
+    def create(self, validated_data):
+        user=self.context['request'].user
+        instance=Debtor(admin_creator=user, **validated_data)
+        instance.save()
+
+        return instance
+
     class Meta:
         model = Debtor
-        fields = '__all__'
+        fields = ('id', 'first_name', 'last_name', 'email', 'iban',
+                  'open_invoices_count', 'overdue_invoices_count', 'paid_invoices_count')
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
