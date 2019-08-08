3439468d3c92434b61a0b8b8eb664fa396bc3594
YaserAlnajjar <kasseryasser@windowslive.com>
2019-08-08 19:47:28 +0300

    Fix settings path

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 # This will set production as default, but we must still set it with an
 # ENV on heroku to ensure that the migrate command runs agains the correct DB
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
