40099612691b3c542be97f9939bf83ec71e97762
YaserAlnajjar <kasseryasser@windowslive.com>
2019-08-10 23:09:43 +0300

    Cleanup main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
 import Vue from 'vue'
-import '@/plugins/vuetify'
-import App from '@/App.vue'
-import router from '@/router'
-import store from "@/store/store"
-import GSignInButton from 'vue-google-signin-button'
-import $backend from '@/backend'
-Vue.prototype.$backend = $backend
-Vue.config.productionTip = false
 
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css' 
+import '@/plugins/vuetify'
+
+import GSignInButton from 'vue-google-signin-button'
+
+import App from '@/App.vue'
+import router from '@/router'
+import store from "@/store/store"
+
+import $backend from '@/backend'
+
+
+Vue.prototype.$backend = $backend
+Vue.config.productionTip = false
+
 Vue.use(Vuetify)
 Vue.use(GSignInButton)
-// Vue.use(VueRouter)
 
 const vue = new Vue({
   router,
